a613c3ad1243c30a7564c0b4c11eb1707e157a57
WildBunnie <wildbunnie2003@gmail.com>
2024-03-25 08:45:54 +0000

    added an endpoint for news

--- WebServer.py
+++ WebServer.py
@@ -2,11 +2,13 @@ from flask import Flask
 import requests
 import json
 from flask_cors import CORS
+import xml.etree.ElementTree as ET
+from utils import striphtml
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/temp")
+@app.route("/ipma/temperature")
 def ipma_temp():
     url = "https://api.ipma.pt/open-data/observation/meteorology/stations/observations.json"
     id_aveiro = "1210702"
@@ -19,3 +21,17 @@ def ipma_temp():
     result = response[key][id_aveiro]["temperatura"]
 
     return str(result) + "º C"
+
+@app.route("/ua/news")
+def ua_news():
+    url = "http://services.sapo.pt/UA/Online/contents_xml?n=1"
+
+    # get the the entire xml
+    response = requests.get(url).content
+    # parse it
+    root = ET.fromstring(response)
+    # find the description
+    content = root[0].find("item").find("description").text
+
+    # remove any embedded html
+    return striphtml(content)
--- utils.py
+++ utils.py
@@ -1,4 +1,9 @@
 import os
+import re
+
+def striphtml(data):
+    p = re.compile(r'<.*?>')
+    return p.sub('', data)
 
 def getFullPath(filename):
     return 'file://' + os.path.abspath(filename)
